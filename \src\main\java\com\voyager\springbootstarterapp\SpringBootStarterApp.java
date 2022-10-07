cbede48620aef5bea092a7ddf8963b42ecf0565c
Prashant Mishra <prashantmindia@gmail.com>
2022-10-07 15:55:58 +0530

    Create \src\main\java\com\voyager\springbootstarterapp\SpringBootStarterApp.java

---
+++ \src\main\java\com\voyager\springbootstarterapp\SpringBootStarterApp.java
@@ -0,0 +1,16 @@
+package com.voyager.springbootstarterapp;
+
+/**
+ * @author Prashant Mishra
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterApp.class, args);
+    }
+
+}
